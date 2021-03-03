e8af05e29fccbcf75e1f7c6eb621fde7559daac3
geniusserg <50704370+geniusserg@users.noreply.github.com>
2021-03-03 16:35:35 +0300

    Lab 1: create Matrix class

---
+++ Matrix.kt
@@ -0,0 +1,173 @@
+class Complex(val real: Float, val imag: Float) {
+    constructor(): this(0f, 0f)
+    constructor(num: Float): this(num, 0f)
+    operator fun plus(other: Complex): Complex {
+        return Complex(real + other.real, imag + other.imag)
+    }
+    operator fun minus(other: Complex): Complex{
+        return Complex(real - other.real, imag - other.imag)
+    }
+    operator fun times(other: Complex): Complex{
+        return Complex((real * other.real) + (imag*other.imag*(-1)), (imag*other.real) + (real * other.imag))
+    }
+    override fun toString(): String {
+        var sign = when(imag < 0){
+            true -> "-"
+            false -> "+"
+        }
+        return " $real$sign${Math.abs(imag)}*i "
+    }
+}
+
+
+class Matrix {
+
+    private var matrix: Array<Array<Complex?>>
+    private var rowsNum: Int
+    private var colsNum: Int
+
+    constructor(n: Int, m: Int)
+    {
+        this.rowsNum = m
+        this.colsNum = n
+        matrix = Array(n) { arrayOfNulls(m) }
+        for (i in 0..n-1)
+        {
+            for (j in 0..m-1)
+            {
+                matrix[i][j] = Complex()
+            }
+        }
+    }
+
+    constructor(matrix: Array<Array<Complex>>)
+    {
+        rowsNum = matrix.size
+        colsNum = matrix[0].size
+        this.matrix = Array(rowsNum) { arrayOfNulls(colsNum) }
+        for (i in 0..colsNum-1)
+        {
+            for (j in 0..rowsNum-1)
+            {
+                this.matrix[i][j] =
+                    Complex(matrix[i][j].real, matrix[i][j].imag)
+            }
+        }
+    }
+    
+    constructor(matrix: Array<Array<Float>>)
+    {
+        rowsNum = matrix.size
+        colsNum = matrix[0].size
+        this.matrix = Array(rowsNum) { arrayOfNulls(colsNum) }
+        for (i in 0..colsNum-1)
+        {
+            for (j in 0..rowsNum-1)
+            {
+                this.matrix[i][j] = Complex(matrix[i][j], 0f)
+            }
+        }
+    }
+
+    fun transpose(): Matrix
+    {
+        val x = Matrix(colsNum, rowsNum)
+        for (i in 0..colsNum-1)
+        {
+            for (j in 0..rowsNum-1)
+            {
+                x.setValue(matrix[i][j], j, i)
+            }
+        }
+        return x
+    }
+
+    operator fun plus(x: Matrix): Matrix
+    {
+        val tmp = Matrix(colsNum, rowsNum)
+        for (i in 0..colsNum-1)
+        {
+            for (j in 0..rowsNum-1)
+            {
+                tmp.setValue(matrix[i][j]!! + x.matrix[i][j]!!, i, j)
+            }
+        }
+        return tmp
+    }
+
+    operator fun minus(x: Matrix): Matrix
+    {
+        val tmp = Matrix(colsNum, rowsNum)
+        for (i in 0..colsNum-1)
+        {
+            for (j in 0..rowsNum-1)
+            {
+                tmp.setValue(matrix[i][j]!! - x.matrix[i][j]!!, i, j)
+            }
+        }
+        return tmp
+    }
+
+
+    operator fun times(other: Matrix): Matrix
+    {
+        val tmp = Matrix(colsNum, other.rowsNum)
+        for (i in 0..colsNum-1)
+        {
+            for (j in 0..other.rowsNum-1)
+            {
+                var x = Complex()
+                for (k in 0..other.colsNum-1)
+                {
+                    x = x.plus(this.matrix[i][k]!! * other.getValue(k, j)!!)
+                }
+                tmp.setValue(x, i, j)
+            }
+        }
+        return tmp
+    }
+
+    fun getRowsNum(): Int
+    {
+        return this.rowsNum
+    }
+
+    fun getColsNum(): Int
+    {
+        return this.colsNum
+    }
+
+    fun getValue(i: Int, j: Int): Complex?
+    {
+        return matrix[i][j]
+    }
+
+    fun setValue(x: Complex?, i: Int, j: Int)
+    {
+        matrix[i][j] = x!!
+    }
+
+    fun setValue(x: Float?, i: Int, j: Int)
+    {
+        matrix[i][j] = Complex(x!!)
+    }
+
+    override fun toString(): String
+    {
+        val str = StringBuilder()
+        for (i in 0..colsNum-1)
+        {
+            for (j in 0..rowsNum-1) str.append(matrix[i][j]).append("  ")
+            str.append("\n")
+        }
+        return str.toString()
+    }
+}
+
+fun main(){
+    var matrix:Array<Array<Float>> = Array<Array<Float>>(9) {i -> Array<Float>(9) { j -> 9f}}
+    var aclass = Matrix(matrix)
+    var bclass = Matrix(matrix)
+    var result = aclass * bclass
+    print(result)
+}
